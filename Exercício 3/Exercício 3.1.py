40e1b7144a1f92f528c21fcd2116bce0dc5e78fc
Salvador Domingues <salvadordomingues@gmail.com>
2022-03-09 11:30:07 +0000

    exercise numeration update

--- Exercício 3/Exercício 3.1.py
+++ Exercício 3/Exercício 3.1.py
@@ -1,6 +1,6 @@
-# 3.1
+# 1
 poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / O rio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa..."
 lista = poema.split("/")
 print(lista[4],lista[5])
 
-# 3.2
+# 2
