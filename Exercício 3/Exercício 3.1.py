87a0a1a7341db66121e72db8727724f75b8ee1b1
Salvador Domingues <salvadordomingues@gmail.com>
2022-03-09 11:25:45 +0000

    poema na mesma linha

---
+++ Exercício 3/Exercício 3.1.py
@@ -0,0 +1,2 @@
+poema = 'Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / Ovrio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa...'
+
