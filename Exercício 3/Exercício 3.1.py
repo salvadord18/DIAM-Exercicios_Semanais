105aca57ff89ffbb9d65e97797a63bcc429b07f3
Salvador Domingues <salvadordomingues@gmail.com>
2022-03-09 16:30:38 +0000

    Ex. 3.2 created

--- Exercício 3/Exercício 3.1.py
+++ Exercício 3/Exercício 3.1.py
@@ -1,7 +1,7 @@
+poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / O rio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa..."
+
 # 1
 print("\n-Alínea 1:")
-
-poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / O rio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa..."
 
 print(poema.split("/")[4:6])
 
---
+++ Exercício 3/Exercício 3.2.py
@@ -0,0 +1,5 @@
+poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / O rio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa..."
+
+# 1
+print("\n-Alínea 1:")
+
