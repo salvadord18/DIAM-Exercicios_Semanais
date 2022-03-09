efff5f5483cebcb4c7dbad718cc94231b765ae77
ArthurG32 <95880380+ArthurG32@users.noreply.github.com>
2022-03-09 12:03:56 +0000

    3.1.3

--- Exercício 3/Exercício 3.1.py
+++ Exercício 3/Exercício 3.1.py
@@ -4,5 +4,7 @@ poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao f
 print(poema.split("/")[4:6])
 
 # 2
-
 print(poema.replace(" / ", "\n"))
+
+# 3
+print(poema.replace(" / ", "\n") + "\nLivros sao papeis pintados com tinta. \nEstudar e uma coisa em que esta indistinta \nA distincao entre nada e coisa nenhuma.)
