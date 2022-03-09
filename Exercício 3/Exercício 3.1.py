5e8a746c0097a6f2751619648467a0e856451852
Salvador Domingues <salvadordomingues@gmail.com>
2022-03-09 11:31:50 +0000

    1 simplified

--- Exercício 3/Exercício 3.1.py
+++ Exercício 3/Exercício 3.1.py
@@ -1,8 +1,6 @@
 # 1
 poema = "Ai que prazer / Nao cumprir um dever, / Ter um livro para ler / E nao fazer! / Ler e macada, / Estudar e nada. / Sol doira / Sem literatura / O rio corre, bem ou mal, / Sem edicao original. / E a brisa, essa, / De tao naturalmente matinal, / Como o tempo nao tem pressa..."
-lista = poema.split("/")
-print(lista[4],lista[5])
-#or
-#print(poema.split("/")[4:6])
+
+print(poema.split("/")[4:6])
 
 # 2
