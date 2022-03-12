d0f5e3fb5e85e7c01736565754d0291ef1bbeba7
ArthurG32 <95880380+ArthurG32@users.noreply.github.com>
2022-03-12 12:21:00 +0000

    Update Exercício 3.3.py

--- Exercício 3/Exercício 3.3.py
+++ Exercício 3/Exercício 3.3.py
@@ -1,9 +1,10 @@
 # 1.
 lista = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+tamanho = len(lista)
 aux = 0
-for l in lista:
+for l in range(tamanho-1):
     if lista[l] > lista[l+1]:
         aux = lista[l]
         lista[l] = lista[l+1]
         lista[l + 1] = aux
-print(str(lista))
+print(lista)
