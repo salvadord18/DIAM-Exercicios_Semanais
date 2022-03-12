46a6ad1fb62718bfb9cf15edd86da93fffcbf2a5
mariapc01 <100026819+mariapc01@users.noreply.github.com>
2022-03-12 19:19:47 +0000

    Update Exercício 3.4.py

--- Exercício 3/Exercício 3.4.py
+++ Exercício 3/Exercício 3.4.py
@@ -25,10 +25,10 @@ sortString("olá", "olá")
 #3)
 
 # 4)
-def (string1, string2):
-    vowel = "aeiou"
-    count1 = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
-    count2 = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+def countletters(string1, string2):
+    vowel = "abcdefghijklmnaopqrstuvwsyz"
+    count1 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    count2 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
     for i in string1:
         if i in vowel:
             if (count1[i] != 0):
