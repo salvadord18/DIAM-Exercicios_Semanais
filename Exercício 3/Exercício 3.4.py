9b92358ed2f37a826075fb70492d9d3334b18662
mariapc01 <100026819+mariapc01@users.noreply.github.com>
2022-03-12 19:06:43 +0000

    Update Exercício 3.4.py

--- Exercício 3/Exercício 3.4.py
+++ Exercício 3/Exercício 3.4.py
@@ -7,8 +7,9 @@ def functionname(string1, string2) :
             if i == j:
                 arr2 = s.replace(j, "None")
     for a in arr2:
-        if a != "None":
-            print("Não é possível")
+        if a != "None"7:
+            print("As palavras não são transponíveis")
+
 
 functionname("olá", "adeus")
 
@@ -17,11 +18,32 @@ def sortString(string1, string2):
     string1 = sorted(string1)
     string2 = sorted(string2)
     if string1 == string2:
-        print("É possível")
+        print("As palavras são transponíveis")
 
 sortString("olá", "olá")
 
 #3)
 
 # 4)
+def (string1, string2):
+    vowel = "aeiou"
+    count1 = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+    count2 = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+    for i in string1:
+        if i in vowel:
+            if (count1[i] != 0):
+                count1[i] += 1
+            else:
+                count1[i] = 1
+    for i in string2:
+        if i in vowel:
+            if (count2[i] != 0):
+                count2[i] += 1
+            else:
+                count2[i] = 1
+    for l in count1:
+        for f in count2:
+            if count1[l] != count2[f]:
+                print("As palavras não são transponíveis")
+
 
